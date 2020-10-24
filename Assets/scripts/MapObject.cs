5be51027227ccccddd4d91fbae85140c3c8df372
Alexandr <kiralpa1503@gmail.com>
2020-10-24 18:38:23 +0400

    Initial commit

---
+++ Assets/scripts/MapObject.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MapObject : MonoBehaviour
+{
+    [SerializeField] private Sprite sprite;
+    [SerializeField] GameObject owner;
+    public Image icon;
+
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
