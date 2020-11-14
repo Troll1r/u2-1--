3d7edae5f571dfa5cba5964bb8ad6a143f69ed4d
Alexandr <kiralpa1503@gmail.com>
2020-11-14 19:09:40 +0400

    14.11.2

---
+++ Assets/scripts/MeleeWeapon.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MeleeWeapon : Weapon
+{
+    public override void Attack()
+    {
+        //anim.play
+        print("bam");
+    
+    
+    }
+
+}
---
+++ Assets/scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    Weapon weapon;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        weapon = GetComponentInChildren<Weapon>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            weapon.Attack();
+        
+        
+        }
+
+        
+    }
+}
